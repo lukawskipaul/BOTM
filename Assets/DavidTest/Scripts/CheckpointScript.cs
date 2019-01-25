22e67609383c253ca5fd56116673527dceb81eb9
David Beaudoin <David.Beaudoin@iam.local>
2019-01-25 10:39:21 -0600

    Add Checkpoint Stuff

---
+++ Assets/DavidTest/Scripts/CheckpointScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    private bool isActivated;
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+    }
+}
---
+++ Assets/DavidTest/Scripts/PlayerRespawnScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawnScript : MonoBehaviour
+{
+    private CheckpointScript currentCheckpoint;
+    private Rigidbody rb;
+
+    private void SetCurrentCheckpoint(CheckpointScript newCurrentCheckpoint)
+    {
+        if(currentCheckpoint != null)
+        {
+            currentCheckpoint.SetIsActivated(false);
+        }
+
+        currentCheckpoint = newCurrentCheckpoint;
+        currentCheckpoint.SetIsActivated(true);
+    }
+
+    private void RespawnPlayer()
+    {
+        rb.velocity = Vector3.zero;
+
+        //
+
+        if (currentCheckpoint == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            //Things that need to be reset to intial value will go under here
+            //
+        }
+        else
+        {
+            transform.position = currentCheckpoint.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            SetCurrentCheckpoint(other.GetComponent<CheckpointScript>());
+        }
+    }
+}
