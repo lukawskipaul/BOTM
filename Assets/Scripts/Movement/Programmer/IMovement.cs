6b824112b62e1e8cf17f902de51c168a2e462165
Lavance Strickland <lavance_strickland@yahoo.com>
2019-01-30 11:02:34 -0600

    Deleted all files not pertaining to Ai combat. Added Boss Ai Animation Controller

--- Assets/Scripts/Movement/Programmer/IMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IMovement {
-
-    Vector3 Direction { get; set; }
-    float Speed { get; set; }
-}
