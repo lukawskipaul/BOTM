a191a34d9f00efaf81c1d6667fef9fd71fa49d86
CamHargrave <tvastacarious@gmail.com>
2019-02-01 10:31:16 -0600

    Telekinesis shield
    Shield Spawns on pressing 'd', and disappears after 2 seconds

---
+++ Assets/CamMechanics/Assets/Scripts/LifeTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour
+{
+    [SerializeField]
+    private float lifeTime = 2f;
+
+
+    void Update()
+    {
+        if (lifeTime > 0)
+        {
+            lifeTime -= Time.deltaTime;
+        }
+        if (lifeTime <= 0)
+        {
+            Destruction();
+        }
+    }
+
+    void Destruction()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/CamMechanics/Assets/Scripts/TShield.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TShield : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Spawnpoint;
+    [SerializeField]
+    private GameObject Shield;
+
+
+    void  Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            Instantiate(Shield, Spawnpoint.position,Spawnpoint.rotation);
+        }
+      
+    }
+}
