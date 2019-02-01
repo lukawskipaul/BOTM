df67b6deca594e80cb72682264b55596cef8a7f3
Caleb Flick <calebflick@gmail.com>
2019-02-01 06:47:50 -0600

    damage enemy with collision

--- Assets/CalebTest/Scripts/DamageEnemy.cs
+++ Assets/CalebTest/Scripts/DamageEnemy.cs
@@ -7,36 +7,37 @@ public class DamageEnemy : MonoBehaviour
 {
     [SerializeField]
     private float attackDamage = 25.0f;
-    [SerializeField]
-    private GameObject testEnemy;
+    //[SerializeField]
+    //private GameObject testEnemy;
 
     private Animator anim;
 
     private void Start()
     {
-        anim = this.gameObject.GetComponent<Animator>();
+        anim = this.gameObject.GetComponentInParent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log("Hit: " + other.name);
         bool isValidTarget = other.tag == "Enemy" && anim.GetBool("isAttacking") == true;
 
-        if (true)
+        if (isValidTarget)
         {
             other.gameObject.GetComponent<EnemyHealth>().DamageEnemy(attackDamage);
         }
     }
 
-    void Update()
-    {
-        //TestAttack();
-    }
+    //void Update()
+    //{
+    //    //TestAttack();
+    //}
 
-    private void TestAttack()
-    {
-        if (Input.GetButtonDown("Attack"))
-        {
-            testEnemy.GetComponent<EnemyHealth>().DamageEnemy(attackDamage);
-        }
-    }
+    //private void TestAttack()
+    //{
+    //    if (Input.GetButtonDown("Attack"))
+    //    {
+    //        testEnemy.GetComponent<EnemyHealth>().DamageEnemy(attackDamage);
+    //    }
+    //}
 }
