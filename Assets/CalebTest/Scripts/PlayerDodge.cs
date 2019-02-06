49b79c300364e223b1b96fae994784c650578ca8
Caleb Flick <calebflick@gmail.com>
2019-02-06 14:34:34 -0600

    added bad dodge

---
+++ Assets/CalebTest/Scripts/PlayerDodge.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerDodge : MonoBehaviour
+{
+    [SerializeField]
+    private float dodgeDistance = 100.0f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Dodge()
+    {
+        if (Input.GetButtonDown("Dodge") /*&& isOnGround*/)
+        {
+            rb.AddForce(transform.forward * dodgeDistance, ForceMode.Impulse);
+        }
+
+        //isDodging = true;
+
+        //Implement dodge timer
+        //Implement I-frames counter
+        //Add Dodge Anim
+    }
+}
