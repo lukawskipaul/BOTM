0c4e2e34f15090a893fda0e15eabdf69dc8e53ab
Paul Lukawski <Paul.Lukawski@loop.colum.edu>
2019-02-06 17:56:55 -0600

    DoorRailway.cs, Environmental Trigger, Trigger.cs, TriggerableObject.cs

---
+++ Assets/Scripts/Util/DoorRailway.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRailway : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+    private int current;
+    TriggerableObject Triggerable;
+
+    void Update()
+    { 
+        if (GetComponent<TriggerableObject>().isTriggered == true)
+        {
+            Doorway();
+        }
+       
+    }
+    public void Doorway()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
+    }
+    
+}
---
+++ Assets/Scripts/Util/TriggerableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerableObject : MonoBehaviour
+{
+   public bool isTriggered = false;
+    
+}
