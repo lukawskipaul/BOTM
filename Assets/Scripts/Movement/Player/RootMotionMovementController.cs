31501fa9c7a6a5529a698c1f1ef9c8ddaa41b224
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-02-09 12:43:16 -0600

    Update Movement Animations, Rotate Player with Camera
    Updated the movement blend tree for the player to remove turning animations and tank-y controls.  The player now rotates with the camera when they are moving.

--- Assets/Scripts/Movement/Player/RootMotionMovementController.cs
+++ Assets/Scripts/Movement/Player/RootMotionMovementController.cs
@@ -10,12 +10,17 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider))]
 public class RootMotionMovementController : MonoBehaviour
 {
+    [SerializeField]
+    private int dodgeDistance;
+
     private Animator anim;
-    bool canMove = true;
+    private Rigidbody rb;
+    private bool canMove = true;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -25,9 +30,8 @@ public class RootMotionMovementController : MonoBehaviour
         if (canMove)
         {
             Move();
-            Jump();
+            Dodge();
         }
-
     }
 
     private void Move()
@@ -37,16 +41,25 @@ public class RootMotionMovementController : MonoBehaviour
         //adjusts the float value for "MoveX" denoted in the attached Animator based on Horizontal input
         anim.SetFloat("MoveX", Input.GetAxis("Horizontal"));
 
-        //adjusts the float value for "Turn" denoted in the attached Animator based on Mouse movement
-        //anim.SetFloat("Turn", Input.GetAxis("Mouse X"));
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            //look with Camera
+            transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up);
+            //lock rotation to only the Y axis
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+        }
     }
 
-    private void Jump()
+    private void Dodge()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Dodge") /*&& isOnGround*/)
         {
-            //Sets the Animator Trigger for "Jump"
-            anim.SetTrigger("Jump");
+            anim.SetTrigger("Dodge");
+
+            rb.AddForce(transform.up * 10.0f, ForceMode.Impulse);
+            rb.AddForce(transform.forward * dodgeDistance, ForceMode.Impulse);
+
+            //have i-frames
         }
     }
 
