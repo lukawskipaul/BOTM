05392a7fb77391ec00b7ee1d7157bb47e2c82707
bpaner21 <bernardjohn.paner@loop.colum.edu>
2019-02-13 16:11:09 -0600

    Reintroduced missing AI assets to Programming assets

--- Assets/Strafe_Target.cs
+++ Assets/Strafe_Target.cs
@@ -1,17 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class DeathState : StateMachineBehaviour
+public class Strafe_Target : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        NavMeshAgent agent = animator.GetComponent<NavMeshAgent>();
-        agent.isStopped = true;
-        
-    }
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
