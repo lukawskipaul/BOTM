38d8db5a92c0ec2f5493b185bf129afb67fcd3e0
Nolan Roher <maniacalspy@gmail.com>
2019-02-12 20:58:37 -0600

    LockOn Updated
    Updated the lock on mechanic to lock on to the nearest enemy. Edited Castsphere to take in a string for the tag your searching for in order to reuse it to search for nearby enemies.

--- Assets/Scripts/Power/DetectObject.cs
+++ Assets/Scripts/Power/DetectObject.cs
@@ -7,8 +7,13 @@ public class DetectObject : MonoBehaviour
 {
     // Event that is raised when an object is detected
     public static event Action<GameObject> LevObjectDetected;
+    public static event Action<GameObject> EnemyObjDetected;
     // Event that is rasied when an object is no longer in sight
     public static event Action LevObjectGone;
+    public static event Action EnemyObjGone;
+
+    //A boolean that InputCameraChange modifies to inform this class to run the private method CastSphere
+    public static bool EnemySearchNeeded;
 
     [SerializeField]
     Transform detectPoint;
@@ -20,33 +25,43 @@ public class DetectObject : MonoBehaviour
     private void Update()
     {
         detectVector = detectPoint.position;
-        CastSphere();
+        CastSphere("LevitatableObject");
+        if (EnemySearchNeeded)
+        {
+            CastSphere("Enemy");
+        }
     }
 
     //Spherecast To detect objects We can use telekinesis on, then do a Raycast along the path between the detectVector and the levitation object to make sure it's clear
-    private void CastSphere()
+    private void CastSphere(String FindTag)
     {
         Ray ray = new Ray(detectVector, transform.forward);
         RaycastHit hit;
-        if (Physics.SphereCast(ray, detectRadius, out hit, detectRange))
+        int IgnoreRayCastLayer = 1 << 2;
+        int PlayerLayer = 1 << 9;
+        int IgnoredLayerMask = ~((IgnoreRayCastLayer) | (PlayerLayer));
+        if (Physics.SphereCast(ray, detectRadius, out hit, detectRange, IgnoredLayerMask))
         {
-            if (hit.collider.gameObject.tag == "LevitatableObject")
+            if (hit.collider.gameObject.tag == FindTag)
             {
                 bool pathToObjClear;
-                RaycastHit nonLevObjHit;
+                RaycastHit nonTagObjHit;
                 float distToLevObj = (hit.point - detectVector).magnitude;
-                if (Physics.Raycast(detectVector, hit.point - detectVector, out nonLevObjHit, distToLevObj))
+                if (Physics.Raycast(detectVector, hit.point - detectVector, out nonTagObjHit, distToLevObj, IgnoredLayerMask))
                 {
-                    if (nonLevObjHit.collider.gameObject.tag != "LevitatableObject" && nonLevObjHit.collider.gameObject.tag != "Checkpoint")
+                    if (nonTagObjHit.collider.gameObject.tag != FindTag && nonTagObjHit.collider.gameObject.tag != "Checkpoint")
                     {
-                        Debug.Log("PathBlockedByNonLevObject");
+
+                        Debug.Log("PathBlockedByObject: " + nonTagObjHit.collider.gameObject);
                         pathToObjClear = false;
+                        //the path to the enemy is blocked, give up searching
+                        if (FindTag == "Enemy") EnemySearchNeeded = false;
                     }
                     else
                     {
                         pathToObjClear = true;
-                       
-                        Debug.Log("TeleObjDetected");
+
+                        Debug.Log(FindTag + "Detected");
                     }
                 }
                 else
@@ -55,19 +70,45 @@ public class DetectObject : MonoBehaviour
                 }
                 if (pathToObjClear)
                 {
-                    OnLevObjectDetected(hit.collider.gameObject);
+                    if (FindTag == "LevitatableObject") OnLevObjectDetected(hit.collider.gameObject);
+                    else if (FindTag == "Enemy")
+                    {
+                        OnEnemyObjDetected(hit.collider.gameObject);
+                        //We found an enemy, we don't need to search anymore
+                        EnemySearchNeeded = false;
+                    }
                 }
             }
-            
+
         }
         else
         {
-            Debug.Log("NoTeleObj");
-            OnLevObjectGone();
+            Debug.Log("No" + FindTag);
+            if (FindTag == "LevitatableObject") OnLevObjectGone();
+            if (FindTag == "Enemy")
+            {
+                OnEnemyObjGone();
+                //there was no enemy, don't keep searching
+                EnemySearchNeeded = false;
+            }
         }
     }
 
     // Publish Events
+
+    private void OnEnemyObjDetected(GameObject detObj)
+    {
+        if (EnemyObjDetected != null)
+        {
+            EnemyObjDetected.Invoke(detObj);
+        }
+    }
+
+    private void OnEnemyObjGone()
+    {
+        EnemyObjGone.Invoke();
+    }
+
     private void OnLevObjectDetected(GameObject detObj)
     {
         if (LevObjectDetected != null)
--- Assets/Scripts/Util/InputCameraChange.cs
+++ Assets/Scripts/Util/InputCameraChange.cs
@@ -6,6 +6,8 @@ public class InputCameraChange : MonoBehaviour
 
 {
     [SerializeField] GameObject CM_LockOnCamera;
+    [SerializeField] GameObject CM_LookAtTargetObject;
+    
 
     private Animator anim;
     private bool lockOn;
@@ -25,9 +27,71 @@ public class InputCameraChange : MonoBehaviour
     {
         if (Input.GetButtonDown("LockOn"))
         {
-            lockOn = !lockOn;
-            CM_LockOnCamera.SetActive(lockOn);
-            anim.SetBool("LockOn", lockOn);
+            if (!lockOn)
+            {
+                //if not locked on, tell DetectObject we're looking for an enemy, so it will run DetectObject.CastSphere("Enemy"), which will look for an enemy and set the need to search as false when it's done
+                DetectObject.EnemySearchNeeded = true;
+                //TODO: Determine the best way to test for how far away the player is from the enemy, so that the lock on doesn't go an infinite distance.
+                //Notes:
+                //Continuously searching for an enemy with CastSphere the same way it looks for Levitatable Objects leads to a jerky camera and makes the lock on break when too close to the enemy.
+                //The enemy's sword blocks locking on, since it lacks a tag, so CastSphere isn't told to ignore it like it's told to ignore checkpoints.
+                //Moving the enemy sword to the IgnoreRaycast Layer makes the sword unable to hit the player.
+                //Giving the enemy's sword the Enemy tag means the player would be able to lock onto it.
+            }
+            else ResetLockOnTarget();
+
         }
+    }
+
+    private void UnLockCamera()
+    {
+        Debug.Log("CameraUnlocked");
+        lockOn = false;
+        CM_LockOnCamera.SetActive(lockOn);
+        anim.SetBool("LockOn", lockOn);
+
+    }
+
+    private void LockCamera()
+    {
+        Debug.Log("Camera Locked");
+        lockOn = true;
+        CM_LockOnCamera.SetActive(lockOn);
+        anim.SetBool("LockOn", lockOn);
+    }
+
+    private void SetLockOnTarget(GameObject gameObject)
+    {
+        //we found our LockOn Target, set it as what to target in the cinemachine object
+        Cinemachine.CinemachineTargetGroup CM_TargetGroup = CM_LookAtTargetObject.GetComponent<Cinemachine.CinemachineTargetGroup>();
+                CM_TargetGroup.m_Targets[1] = new Cinemachine.CinemachineTargetGroup.Target();
+                CM_TargetGroup.m_Targets[1].weight = 1;
+                CM_TargetGroup.m_Targets[1].radius = 1;
+                CM_TargetGroup.m_Targets[1].target = gameObject.transform;
+                LockCamera();
+        
+    }
+
+    private void ResetLockOnTarget()
+    {
+        //remove the target from the cinemachine lock on list and unlocks the camera
+        
+        Cinemachine.CinemachineTargetGroup CM_TargetGroup = CM_LookAtTargetObject.GetComponent<Cinemachine.CinemachineTargetGroup>();
+        CM_TargetGroup.m_Targets[1] = new Cinemachine.CinemachineTargetGroup.Target();
+        UnLockCamera();
+         
+
+    }
+
+    private void OnEnable()
+    {
+        DetectObject.EnemyObjDetected += SetLockOnTarget;
+        DetectObject.EnemyObjGone += ResetLockOnTarget;
+    }
+
+    private void OnDisable()
+    {
+        DetectObject.EnemyObjDetected -= SetLockOnTarget;
+        DetectObject.EnemyObjGone -= ResetLockOnTarget;
     }
 }
