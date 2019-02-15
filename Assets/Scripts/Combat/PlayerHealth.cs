a077ed201e2707d2b7c92d0e06afa2f60463a94a
Caleb Flick <calebflick@gmail.com>
2019-02-15 06:10:18 -0600

    added deleted work

--- Assets/Scripts/Combat/PlayerHealth.cs
+++ Assets/Scripts/Combat/PlayerHealth.cs
@@ -12,23 +12,31 @@ public class PlayerHealth : MonoBehaviour
     private int maxHealth = 100;
 
     private int currentHealth;
+    private bool isInvulnerable;
 
-    void Start()
+    private void Awake()
     {
         currentHealth = maxHealth;
+        isInvulnerable = false;
+    }
 
+    private void Start()
+    {
         UpdateHealthBar();
     }
 
-    void Update()
+    private void Update()
     {
         UpdateHealthBar();
     }
 
     public void DamagePlayer(int amount)
     {
-        /* Damages player by enemy attack amount */
-        currentHealth -= amount;
+        /* Damages player by enemy attack amount if not during iframe */
+        if (!isInvulnerable)
+        {
+            currentHealth -= amount;
+        }
 
         /* Player dies when health reaches 0 */
         if (currentHealth <= 0)
@@ -49,9 +57,21 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    void UpdateHealthBar()
+    private void UpdateHealthBar()
     {
         /* Updates health bar with current health */
         healthBar.value = currentHealth / maxHealth;
+    }
+
+    /* Called at specific dodge animation frame to make player invulnerable */
+    public void MakeInvulnerable()
+    {
+        isInvulnerable = true;
+    }
+
+    /* Called at specific dodge animation frame to make player vulnerable */
+    public void MakeVulnerable()
+    {
+        isInvulnerable = false;
     }
 }
