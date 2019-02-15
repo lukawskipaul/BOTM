aef765d65a6896f63e3fc28b3dd2c422f42109a7
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2019-02-15 09:21:26 -0600

    Remove the old project

--- Assets/Scripts/Camera/Programmers/Camera/ICameraMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICameraMovement{
-    GameObject targetObject { get; set; }
-    void Start(Transform cameraTransform);
-    void LateUpdate(Transform cameraTransform);
-}
