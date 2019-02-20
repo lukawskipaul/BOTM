bb6003f03e7f4719a154e5b3c6bbd986abb78551
Spidakid <lavance.strickland>
2019-02-20 17:35:27 -0600

    Updated Yawn phase of Boss Controller

--- Assets/StartPhase.cs
+++ Assets/StartPhase.cs
@@ -20,7 +20,7 @@ public class StartPhase : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //animator.SetBool("WakeUp",true);
+        //animator.SetBool("isAwake", true);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
