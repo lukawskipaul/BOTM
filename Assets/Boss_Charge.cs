79a9015b4bcbcce26542adec9f58015e5f24f979
bpaner21 <bernardjohn.paner@loop.colum.edu>
2019-02-20 16:35:56 -0600

    Completed initial JumpBack code and fixed boss rotation when turning towards player object

--- Assets/Boss_Charge.cs
+++ Assets/Boss_Charge.cs
@@ -10,6 +10,9 @@ public class Boss_Charge : StateMachineBehaviour
     BossAI bossAI;
     NavMeshAgent bossNavMeshAgent;
 
+    float lookRotationSpeed;
+    Quaternion lookAtPlayer;
+
     GameObject target;  // used for debugging and visualization
                         // comment out in release version of game
 
@@ -23,13 +26,15 @@ public class Boss_Charge : StateMachineBehaviour
         bossAI = boss.GetComponent<BossAI>();
         player = bossAI.Player;
         bossNavMeshAgent = bossAI.BossNavMeshAgent;
+        lookRotationSpeed = bossAI.LookRotationSpeed;
 
         target = bossAI.Target; // used for debugging and visualization
                                 // comment out in release version of game
 
         // rotate boss to face player
-        boss.transform.LookAt(player.transform);
+        lookAtPlayer = Quaternion.LookRotation(player.transform.position - boss.transform.position);
 
+        
         // calculate vector distance between boss and player
         targetPosition = player.transform.position - boss.transform.position;
 
@@ -38,22 +43,29 @@ public class Boss_Charge : StateMachineBehaviour
 
         // set boss destination
         targetPosition += boss.transform.position;
-        bossNavMeshAgent.SetDestination(targetPosition);
 
         target.transform.position = targetPosition; // used for debugging and visualization
                                                     // comment out in release version of game
-
+        
         // make boss faster
         bossNavMeshAgent.speed *= bossAI.ChargeSpeedScalar;
         bossNavMeshAgent.acceleration *= bossAI.ChargeAccelerationScalar;
-        bossNavMeshAgent.angularSpeed *= bossAI.ChargeAngularSpeedScalar;
+        bossNavMeshAgent.angularSpeed *= bossAI.ChargeAngularSpeedScalar;//*/
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // rotate boss to look at player, then charge
+        if (Quaternion.Dot(boss.transform.rotation, lookAtPlayer) < 0.99f)
+        {
+            boss.transform.rotation = Quaternion.Slerp(boss.transform.rotation, lookAtPlayer, lookRotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            bossNavMeshAgent.SetDestination(targetPosition);
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
