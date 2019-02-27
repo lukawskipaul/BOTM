6778f24c31e62ac6b2b6567afc80bd71d43db42a
Lavance Strickland <lavance_strickland@yahoo.com>
2019-02-27 01:18:06 -0600

    Boss States/Hitbox scripts

---
+++ Assets/Scripts/AI/States/Boss States/Boss_AttackIdle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss_AttackIdle : StateMachineBehaviour
+{
+    Quaternion lookpos;
+    float lookSpeed = 3;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Select random attack action
+        animator.SetInteger("AttackChoice", (int)(Random.value * 8));
+        //Set look position to player
+        lookpos = Quaternion.LookRotation(animator.GetComponent<BossEnemyMono>().Player.transform.position - animator.transform.position);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.transform.rotation = Quaternion.Slerp(animator.transform.rotation,lookpos,lookSpeed * Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //animator.gameObject.transform.rotation = Quaternion.LookRotation(lookpos, Vector3.up);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
