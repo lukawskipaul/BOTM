09023dfcc0f0959fbb0643cb4df191dd871bca5c
BrendanWascher <brendanwascher@hotmail.com>
2019-03-02 14:12:21 -0600

    2019-3-6 demo part 1

--- Assets/UI/UI Scripts/PauseMenuManager.cs
+++ Assets/UI/UI Scripts/PauseMenuManager.cs
@@ -2,22 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
     public GameObject journalMenu;
     public GameObject pauseMenu;
     public GameObject controlsCanvas;
-    
-    public string MainMenuScene;
-    public string DemoScene;
+
     public string journalInput;
     public string pauseInput;
     public string controlsInput;
 
     public bool paused;
-    public bool journalOpen;
 
     void Start()
     {
@@ -32,7 +28,7 @@ public class PauseMenuManager : MonoBehaviour
     {
         if (Input.GetButtonDown(pauseInput))
         {
-            if(paused == false && journalOpen == false)
+            if(paused == false)
             {
                 PauseGame();
             }
@@ -41,21 +37,16 @@ public class PauseMenuManager : MonoBehaviour
             {
                 ClosePauseMenu();
             }
-
-            else if(journalOpen == true)
-            {
-                CloseJournal();
-            }
         }
 
-        if (Input.GetButtonDown(journalInput))
+        if (Input.GetButton(journalInput))
         {
-            if (paused == false && journalOpen == false)
+            if (paused == false)
             {
                 OpenJournal();
             }
 
-            else if (journalOpen == true)
+            else if (paused == true)
             {
                 CloseJournal();
             }
@@ -81,8 +72,6 @@ public class PauseMenuManager : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
-
-
     public void ClosePauseMenu()
     {
         Time.timeScale = 1;
@@ -97,7 +86,7 @@ public class PauseMenuManager : MonoBehaviour
     {
         Time.timeScale = 0;
         journalMenu.gameObject.SetActive(true);
-        journalOpen = true;
+        paused = true;
 
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
@@ -106,59 +95,9 @@ public class PauseMenuManager : MonoBehaviour
     {
         Time.timeScale = 1;
         journalMenu.gameObject.SetActive(false);
-        journalOpen = false;
+        paused = false;
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    public void QuitToMainButton()
-    {
-        Time.timeScale = 1;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-
-        SceneManager.LoadScene(MainMenuScene);
-    }
-
-    public void RestartLevel()
-    {
-        Time.timeScale = 1;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-
-        SceneManager.LoadScene(DemoScene);
-    }
-
-    //Texture for the screen to fade from.
-    public Texture2D fadeOutTexture;
-    [Tooltip("Speed of the screen fade. Lower is Slower")]
-    public float fadeSpeed = 1.0f;
-
-    private int drawDepth = -1000;
-    private float alpha = 1.0f;
-    private int fadeDirection = -1;
-
-    private void OnGUI()
-    {
-        alpha += fadeDirection * fadeSpeed * Time.deltaTime;
-
-        alpha = Mathf.Clamp01(alpha);
-
-        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
-        GUI.depth = drawDepth;
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
-    }
-
-    public float BeginFade(int direction)
-    {
-        fadeDirection = direction;
-        return (fadeSpeed);
-    }
-
-    private void OnLevelWasLoaded()
-    {
-        BeginFade(-1);
-    }
-
 }
