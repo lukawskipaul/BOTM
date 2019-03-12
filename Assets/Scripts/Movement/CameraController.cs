491d9977bd262d3e9a47b80a1e3bced0b70f0b61
Paul Lukawski <Paul.Lukawski@loop.colum.edu>
2019-03-11 21:36:02 -0500

    new CharacterController.cs and CameraController.cs

---
+++ Assets/Scripts/Movement/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float looksmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 6, -8);
+    public float xTilt = 10;
+
+    Vector3 destination = Vector3.zero;
+    CharacterController charcontroller;
+    float rotateVel = 0;
+
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t)
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<CharacterController>())
+            {
+                charcontroller = target.GetComponent<CharacterController>();
+            }
+            else
+                Debug.LogError("Camera's target needs character controller.");
+        }
+        else
+            Debug.LogError("Camera needs a target.");
+    }
+
+    void LateUpdate()
+    {
+        //moving
+        MoveToTarget();
+        //rotating
+        LookAtTarget();
+    }
+
+    void MoveToTarget()
+    {
+        destination = charcontroller.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, looksmooth); // smoothDampAngle = lerp, but smoother.
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+}
---
+++ Assets/Scripts/Movement/CharacterController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float inputDelay = 0.1f;// input DeadZone
+    public float forwardVelocity = 12;
+    public float rotateVelocity = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rb;
+    float forwardInput, turnInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rb = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("Character does not have a Rigidbody.");
+
+        forwardInput = turnInput = 0;
+    }
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Veritcal");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Walk()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        {
+            //move
+            rb.velocity = transform.forward * forwardInput * forwardVelocity;
+        }
+        else
+            //zero velocity if less then deadzone
+            rb.velocity = Vector3.zero;
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(rotateVelocity * turnInput * Time.deltaTime, Vector3.up);
+        }
+            transform.rotation = targetRotation;
+    }
+}
