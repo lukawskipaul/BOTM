aec1c610e041eae23f552e068491930966545ca0
apod1010 <32077837+apod1010@users.noreply.github.com>
2019-03-17 12:21:56 -0500

    Lighting final pass, UI Main Menu

---
+++ Assets/Scripts/BasicCloudMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCloudMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(speed, 0, 0);
+    }
+}
