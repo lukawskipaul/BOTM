e70817558fbe3292500f81a81aea9600f400e535
ColeMcdaniel <colemandew17@gmail.com>
2019-04-03 17:45:25 -0500

    Fixed Death, death anim, locked AI enemy

--- Assets/Scripts/Power/Telekinesis.cs
+++ Assets/Scripts/Power/Telekinesis.cs
@@ -171,9 +171,12 @@ public class Telekinesis : MonoBehaviour
 
     private void DropObject()
     {
-        objectRigidBody.useGravity = true;
-        currentTKObject.SetNeutral();
-        ResetTK();
+        if (isLiftingObject)
+        {
+            objectRigidBody.useGravity = true;
+            currentTKObject.SetNeutral();
+            ResetTK();
+        }
     }
 
     private void CheckDistance()
